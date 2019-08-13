36bc6d7a9637d5e7d27a79c80347ec06e64a3a05
Winster-cheng <1105874777@qq.com>
2019-08-13 09:59:37 +0800

    修稿dubbo-api

--- dubbo-api/src/main/java/com/dubbo/api/domian/User.java
+++ dubbo-api/src/main/java/com/dubbo/api/domian/User.java
@@ -1,17 +1,8 @@
 package com.dubbo.api.domian;
 
-
-
-
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
 import java.io.Serializable;
 
-@Getter
-@Setter
-@ToString
+
 public class User implements Serializable {
     private Integer id;
     private String name;
@@ -27,5 +18,43 @@ public class User implements Serializable {
         this.address = address;
     }
 
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
